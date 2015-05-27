9f18618b096efc552c494dbc6786a15953ce3d45
edwardshen123 <e@eshen.me>
2015-05-27 09:26:17 -0400

    updated

---
+++ budgetGrabber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class budgetGrabber {
+
+    public static void read(String filename, monthlyBudgetBuilder builder) {
+    }
+
+    public static void write(String filename, monthlyBudgetBuilder builder) {
+    }
+}
