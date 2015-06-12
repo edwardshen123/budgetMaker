5b87ed93f2202fb453dc29ed6f592396d0565bda
Tommy Li <tommyli12345@gmail.com>
2015-06-12 13:16:42 -0400

    Working on it....

--- execute.java
+++ execute.java
@@ -10,6 +10,18 @@ import java.io.*;
 import java.util.*;
 
 public class execute {
+
+    //Taken from http://www.mkyong.com/java/how-to-traverse-a-directory-structure-in-java/
+    public static void displayIt(File node){
+	System.out.println(node.getAbsoluteFile());
+	if(node.isDirectory()){
+	    String[] subNote = node.list();
+	    for(String filename : subNote){
+		displayIt(new File(node, filename));
+	    }
+	}
+    }
+
     public static void main(String[] args) {
 	Scanner ans = new Scanner(System.in);
 	String whatdo;
@@ -17,9 +29,20 @@ public class execute {
 	    System.out.println("Would you like to load or create a new expense? (Type 'load' or 'create')");
 	    whatdo = ans.nextLine();
 	    if (whatdo.equals('load')) {
-	        budgetGrabber b = new budgetGrabber();
-		monthlyBudgetBuilder m = b.read(filename);
-		
+		displayIt(new File("C:\\Downloads"));
+		System.out.println("Please choose a txt file.");
+		String filename = ans.nextLine();
+		String[] file = filename.split(".");
+		boolean loaded = false;
+		while (loaded == false) {
+		    if (file[file.length - 1].equals("txt")) {
+			budgetGrabber b = new budgetGrabber();
+			monthlyBudgetBuilder m = b.read(filename);
+		    }
+		    else {
+			System.out.println("Invalid file. Please choose another.");
+		    }
+		}
 	    }
 	    else if (whatdo.equals('create')) {
 
